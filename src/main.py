fb4a683375cb179b086170e2531f634718e76e65
Dr. Maxim Orlovsky <dr.orlovsky@gmail.com>
2017-06-05 19:06:24 +0300

    Works with masternode

--- src/main.py
+++ src/main.py
@@ -4,12 +4,12 @@ from broker import Broker, BrokerConfig, IPFSConfig, MWAPIConfig
 from eth_connector import EthConfig
 
 CONFIGS_MWAPI = {
-    'DUMB': MWAPIConfig(host='[::]', port=50051, max_conns=10)
+    'LOCAL': MWAPIConfig(host='[::]', port=50051, max_conns=10)
 }
 
 CONFIGS_ETH = {
-    'LOCAL': EthConfig(server='127.0.0.1', port=80, contract=''),
-    'INFURA': EthConfig(server='https://ropsten.infura.io', port=441, contract='')
+    'LOCAL': EthConfig(server='http://127.0.0.1', port=8545, contract='0x33c9d18fb98e08fe73262e333bcbf428ebedbeff'),
+    'INFURA': EthConfig(server='https://ropsten.infura.io', port=443, contract='')
 }
 
 CONFIGS_IPFS = {
@@ -19,9 +19,9 @@ CONFIGS_IPFS = {
 
 CONFIGS = {
     'TEST_INFRA': BrokerConfig(
-        mwapi=CONFIGS_MWAPI['DUMB'],
+        mwapi=CONFIGS_MWAPI['LOCAL'],
         ipfs=CONFIGS_IPFS['LOCAL'],
-        eth=CONFIGS_ETH['INFURA'],
+        eth=CONFIGS_ETH['LOCAL'],
         max_tasks=10,
         ver_ma=0,
         ver_mi=1,
