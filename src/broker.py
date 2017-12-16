f9ca04e35d18afa54a4fe848b87d28fb800204d8
Dr. Maxim Orlovsky <orlovsky@pandora.foundation>
2017-12-16 19:23:08 +0200

    Initial implementation of Ethereum network interface

--- src/broker.py
+++ src/broker.py
@@ -2,115 +2,67 @@ import logging
 import traceback
 from collections import namedtuple
 
-from mn_connector import *
-from processor import *
-from verificator import Verificator
+from singleton import *
+from eth import *
+from webapi.webapi import *
 
-from neuromwapi.messages import *
-from neuromwapi.services import *
+# Configuration object designed in form of named tuple (common style for all configuration in the project).
+# The reason of using named tuples instead of dict is to get compile-time errors in case of misspelled config keys.
+BrokerConfig = namedtuple('BrokerConfig', 'eth webapi')
 
-
-BrokerConfig = namedtuple('BrokerConfig', 'ipfs eth mwapi max_tasks ver_ma ver_mi patch agent')
-
-
-class BusyError (Exception):
-    pass
-
-
-class Broker (WorkerServicer, ProcessorCallback):
+class Broker (Singleton, EthDelegate, WebDelegate):
+    """
+    Broker manages all underlying services/threads and arranges communications between them. Broker directly manages
+    WebAPI and Ethereum threads and provides delegate interfaces for capturing their output via callback functions.
+    This is done via implementing `EthDelegate` and `WebDelegate` abstract classes.
+    """
 
     def __init__(self, config: BrokerConfig):
+        """
+        Instantiates Broker object and its members, but does not initiates them (network interfaces are not created/
+        bind etc). Broker follows two-step initialization pattern (`Broker(...)` followed by `broker.run` call.
+
+        :param config: Configuration named tuple `BrokerConfig` containing also configuration of all nested services
+        (WebAPI, Ethereum connector)
+        """
+
+        # Calling singleton init preventing repeated class instantiation
+        super().__init__()
+
+        # Initializing logger object
+        self.logger = logging.getLogger("broker")
+
+        # Saving config
         self.config = config
 
-        # Set up masternode connections
-        self.mn_connector = MNConnector(self, self.config.mwapi)
+        # Instantiating services objects
+        self.eth = Eth(config=self.config.eth, delegate=self)
+        self.api = WebAPI(config=self.config.webapi, delegate=self)
 
-        self.processors = []
-        self.tasks = {}
-        print('Starting verification service...')
-        self.verificator = Verificator(self.config.eth)
-        print('Verification service started successfully')
+    def run(self) -> bool:
+        """
+        Starts all necessary interfaces (WebAPI, Ethereum and its underlying interfaces). Fails if any of them failed.
 
-    def run(self):
-        print("Starting masternode interface...")
-        self.mn_connector.serve()
-        print("Masternode interface started successfully")
+        :return: Success or failure status as a bool value
+        """
 
-    def spawn_processor(self) -> Processor:
-        if len(self.processors) >= self.config.max_tasks:
-            logging.warning("Can't instantiate processor, number of tasks is to high")
-            raise BusyError
-        print("Instantiating processor...")
-        processor = Processor(self, ipfs_config=self.config.ipfs)
-        print("New processor instantiated successfully")
-        self.processors.append(processor)
-        return processor
+        # Trying to bind web api port (to fail early before trying everything else more complex)
+        self.logger.debug("Statring api...")
+        if not self.api.bind():
+            self.logger.error("Can't bind to Web API port, shutting down")
+            return False
 
-    #
-    # Masternode-Worker RPC implementation
-    #
+        # Now trying to connect Ethereum node JSON RPC interface
+        self.logger.debug("API started successfully")
+        self.logger.debug("Starting broker...")
+        if not self.eth.bind():
+            self.logger.error("Can't connect Ethereum network, shutting down")
+            return False
 
-    def ping(self, request, context):
-        print("Got ping request from masternode.")
-        return VersionInfo(major=self.config.ver_ma,
-                           minor=self.config.ver_mi,
-                           patch=self.config.patch,
-                           agent=self.config.agent)
+        # Since all necessary network environments are available for now we can run the services as a separate threads
+        self.api.run()
+        self.eth.run()
 
-    def suggest_peers(self, request, context):
-        # TODO: Implement
-        # Current response: I don't know anybody else :)
-        return PeersList()
+        self.logger.debug("Broker started successfully")
 
-    def cognite_batch(self, request: CognitionRequest, context) -> CognitionResponse:
-        print("Got cognite_batch request from masternode")
-        # Verifying integrity, validity and legality
-        if not self.verificator.verify_masternode(request.pub_key, request.signed_message):
-            return CognitionResponse(
-                accepted=False,
-                decline_info=DeclinedTask(reason=DeclineReason.Value('UNVERIFIED'),
-                                          message="Masternode didn't pass verification")
-            )
-        if not self.verificator.verify_neurocontract(request.contract_address):
-            return CognitionResponse(
-                accepted=False,
-                decline_info=DeclinedTask(reason=DeclineReason.Value('UNVERIFIED'),
-                                          message="Contract didn't pass verification")
-            )
-
-        task_id, time_est = None, None
-        decline_reason = None
-        # Run batch processor
-        try:
-            processor = self.spawn_processor()
-            task_id, time_est = processor.cognite_batch(
-                request.arch_address, request.model_address, request.data_address
-            )
-            self.tasks[task_id] = processor
-        except BusyError:
-            decline_reason = DeclinedTask(reason=DeclineReason.Value('BUSY'), message="Too many active tasks already")
-        except IPFSError as ipfs:
-            decline_reason = DeclinedTask(reason=DeclineReason.Value('IPFS_ERROR'), message=ipfs.message)
-        except ModelInconsistencyError:
-            decline_reason = DeclinedTask(reason=DeclineReason.Value('BROKEN_MODEL'), message="Model file is broken")
-        except DataInconsistencyError:
-            decline_reason = DeclinedTask(reason=DeclineReason.Value('BROKEN_DATA'), message="Data file is broken")
-        except Exception as ex:
-            traceback.print_exc()
-            decline_reason = DeclinedTask(reason=DeclineReason.Value('INTERNAL_ERROR'),
-                                          message="Internal exception of type %s" % type(ex))
-        except:
-            decline_reason = DeclinedTask(reason=DeclineReason.Value('INTERNAL_ERROR'), message="Unknown internal error")
-        finally:
-            if decline_reason is not None:
-                logging.warning(decline_reason.message)
-                return CognitionResponse(accepted=False, decline_info=decline_reason)
-
-        return CognitionResponse(accepted=True, task_info=AcceptedTask(task_id=task_id, time_estimate=time_est))
-
-    def batch_status(self, request, context) -> BatchStatus:
-        processor = self.tasks[request.task_id]
-        if processor is not None:
-            return BatchStatus(active=True, time_estimate=processor.get_time_estimate())
-        else:
-            return BatchStatus(active=False, time_estimate=0)
+        return True
