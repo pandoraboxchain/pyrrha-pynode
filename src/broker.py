edbf13af194b68b6c775128ca6c0145902b368e3
Korostelyov Sergey <korostelyov@qoderoom.com>
2018-01-18 12:16:23 +0200

    - reset broker.py dependencies

--- src/broker.py
+++ src/broker.py
@@ -1,14 +1,14 @@
 import sys
 import logging
-import time
 from threading import Thread
-from os.path import exists
 from typing import Union
 
-from patterns.singleton import *
-from node.worker_node import *
-from job.cognitive_job import *
-from processor.processor import *
+from job.cognitive_job import CognitiveJobDelegate, CognitiveJob
+from node.worker_node import WorkerNodeDelegate, WorkerNode
+from patterns.singleton import Singleton
+from processor.processor import ProcessorDelegate
+from eth.eth_connector import EthConnector
+from processor.processor import Processor
 
 
 class Broker(Singleton, Thread, WorkerNodeDelegate, CognitiveJobDelegate, ProcessorDelegate):
